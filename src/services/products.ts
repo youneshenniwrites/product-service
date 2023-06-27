d7c7f3d890d6c0f0d0c3f783d520321f97193adc
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-27 21:33:16 +0100

    define services files

---
+++ src/services/products.ts
@@ -0,0 +1,23 @@
+class Products {
+  constructor() {}
+
+  async createProducts() {
+    return "CREATED";
+  }
+
+  async getProducts() {
+    return "GETALL";
+  }
+
+  async getProduct() {
+    return "GET";
+  }
+
+  async editProduct() {
+    return "EDIT";
+  }
+
+  async deleteProduct() {
+    return "DELETE";
+  }
+}
